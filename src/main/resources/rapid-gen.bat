b3ad562995d5376ee47654d6d596f03e2312589c
renfufei <renfufei@qq.com>
2016-03-30 14:48:54 +0800

    title rapid-generator代码生成器

--- src/main/resources/rapid-gen.bat
+++ src/main/resources/rapid-gen.bat
@@ -2,6 +2,7 @@
 @echo 2. 生成器的主要配置文件为generator.xml,里面修改数据库连接属性
 @echo 3. template目录为代码生成器的模板目录,可自由调整模板的目录结构
 
+@title rapid-generator代码生成器
 @set classpath=%classpath%;.;.\lib\*;.\lib\rapid-generator.jar;.\lib\freemarker.jar;.\lib\h2-1.2.137.jar;.\lib\log4j-1.2.15.jar;.\lib\mysql-connector-java-5.0.5-bin.jar;.\lib\ojdbc14.jar;.\lib\postgresql-8.4-701.jdbc3.jar;.\lib\sqljdbc.jar
 
 @set PATH=%JAVA_HOME%\bin;%PATH%;
